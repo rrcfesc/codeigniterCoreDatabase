718d09bbf4b248247936078da33c455cb5dbaee4
Ricardo Jesus Ruiz Cruz <kasparov@Mac-mini-de-Ricardo.local>
2017-12-09 08:34:11 -0600

    Se agrega la informacion de composer

--- config/production.php
+++ config/production.php
@@ -1,2 +1,22 @@
 <?php
-return array();
+return array(
+    'dsn'	=> '',
+    'hostname' => '127.0.0.1',
+    'username' => 'travis',
+    'password' => 'mysql',
+    'database' => 'magento',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => TRUE,
+    'db_debug' => 'production',
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
